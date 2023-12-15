ba7477026367258a8112b04543538adfa2988b7f
mansi-panwar <manssiipanwarr@gmail.com>
2023-12-15 17:49:48 +0530

    added deploy workflow

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -11,7 +11,7 @@ const Hero = () => {
         <button
           type='button'
           onClick={() =>
-            window.open("https://github.com/TidbitsJS/Summize", "_blank")
+            window.open("https://github.com/mansi-panwar/Ai_summarizer", "_blank")
           }
           className='black_btn'
         >
